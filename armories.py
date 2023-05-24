62c255dc7b095915e7cf0a59f9fef9e5201ab010
Blen <blenbre777@naver.com>
2023-05-25 02:25:00 +0900

    Function-캐릭터 관련 함수 추가

---
+++ armories.py
@@ -0,0 +1,100 @@
+import requests
+from lostArkApi import Token
+
+class armories:
+    headers = {
+            'accept': 'application/json',
+            'authorization' : Token
+        }
+    def __init__(self, name):
+        self.name = name
+    
+    #모든 정보
+    def characters(self):
+        url = f'https://developer-lostark.game.onstove.com/armories/characters/{self.name}'
+
+        response = requests.get(url,headers=self.headers)
+        jsonObject = response.json()
+
+        print(response)
+        print(jsonObject)
+
+    #장착한 아이템
+    def profile(self):
+        url = f'https://developer-lostark.game.onstove.com/armories/characters/{self.name}/profiles'
+
+        response = requests.get(url,headers=self.headers)
+        jsonObject = response.json()
+
+        print(response)
+        print(jsonObject)
+
+    #장비
+    def equipment(self):
+        url = f'https://developer-lostark.game.onstove.com/armories/characters/{self.name}/equipment'
+
+        response = requests.get(url,headers=self.headers)
+        jsonObject = response.json()
+
+        print(response)
+        print(jsonObject)
+
+    #아바타
+    def avatars(self):
+        url = f'https://developer-lostark.game.onstove.com/armories/characters/{self.name}/avatars'
+
+        response = requests.get(url,headers=self.headers)
+        jsonObject = response.json()
+
+        print(response)
+        print(jsonObject)
+    
+    #스킬 
+    def combat_skills(self):
+        url = f'https://developer-lostark.game.onstove.com/armories/characters/{self.name}/combat-skills'
+
+        response = requests.get(url,headers=self.headers)
+        jsonObject = response.json()
+
+        print(response)
+        print(jsonObject)
+
+    #카드
+    def card(self):
+        url = f'https://developer-lostark.game.onstove.com/armories/characters/{self.name}/cards'
+
+        response = requests.get(url,headers=self.headers)
+        jsonObject = response.json()
+
+        print(response)
+        print(jsonObject)
+    
+    #보석 
+    def gems(self):
+        url = f'https://developer-lostark.game.onstove.com/armories/characters/{self.name}/gems'
+
+        response = requests.get(url,headers=self.headers)
+        jsonObject = response.json()
+
+        print(response)
+        print(jsonObject)
+
+    #증명의 전장 
+    def colosseums(self):
+        url = f'https://developer-lostark.game.onstove.com/armories/characters/{self.name}/colosseums'
+
+        response = requests.get(url,headers=self.headers)
+        jsonObject = response.json()
+
+        print(response)
+        print(jsonObject)
+    
+    #수집품 요약 
+    def collectibles(self):
+        url = f'https://developer-lostark.game.onstove.com/armories/characters/{self.name}/collectibles'
+
+        response = requests.get(url,headers=self.headers)
+        jsonObject = response.json()
+
+        print(response)
+        print(jsonObject)
