d4df4ded73258a2a51b6941c255d54bfe4ff9f12
Blen <blenbre777@naver.com>
2023-05-25 02:25:12 +0900

    Function-경매장 추가

---
+++ auctions.py
@@ -0,0 +1,20 @@
+import requests
+from lostArkApi import Token
+
+class auctions:
+    def __init__(self,options):
+        self.options = options
+    
+    #경매장 검색 옵션 반환
+    def auctions_serch():
+        headers = {
+            'accept': 'application/json',
+            'authorization' : Token
+        }
+        url = 'https://developer-lostark.game.onstove.com/auctions/{self.options}'
+
+        response = requests.get(url,headers=headers)
+        jsonObject = response.json()
+
+        print(response)
+        print(jsonObject)
